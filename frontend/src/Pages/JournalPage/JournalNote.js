52dbc08a87a00831d79b8c7d30f26bdf524dcba6
Bhaskar Kapri <kapri.b@somaiya.edu>
2023-12-14 00:43:10 +0530

    journal ui design updated

--- frontend/src/Pages/JournalPage/JournalNote.js
+++ frontend/src/Pages/JournalPage/JournalNote.js
@@ -8,12 +8,9 @@ const JournalNote = () => {
     return (
     <div className="journal-note-container">
 
-        
-        <input className="note-heading" type="text" id="title" name="title" placeholder="Title..."/>
-    
         <textarea className="note-content" id="content" name="content" rows="4"  placeholder="Three things you are grateful for..."></textarea>
-    
-        <button className="note-submit-btn" type="submit">Submit</button>
+        
+        <button className="note-submit-btn" type="submit">Save</button>
       
     </div>
     )
--- frontend/src/Pages/JournalPage/JournalPage.js
+++ frontend/src/Pages/JournalPage/JournalPage.js
@@ -3,10 +3,23 @@
 import React from "react"
 import "./JournalPage.css"
 import JournalNote from "./JournalNote"
+import journalArt from "../../assets/images/journal-art.jpg"
+
+
 
 const JournalPage = () => {
     
     const days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+    const journalQuotes = [
+        "Take a moment to reflect on your day. What are you grateful for? What challenges did you face?",
+        "Even a short note is better than no note at all. Give yourself the gift of reflection.",
+        "Today is a blank canvas. Write something to paint it with your thoughts and feelings.",
+        "Write down something that made you smile today. Capture the little joys of life.",
+        "Journaling is a powerful tool for self-discovery. Start writing to learn more about yourself.",
+        "Don't let today pass without saying hello to yourself. Write your first note."
+    ]
+
+    const [quote,setQuote] = React.useState(journalQuotes[Math.floor(Math.random()*6)])
     const [todayDate,setTodayDate] = React.useState({
         date: new Date().getDate(),
         day:new Date().getDay(),
@@ -14,11 +27,11 @@ const JournalPage = () => {
         year:new Date().getFullYear()
     })
     const [weekDates,setWeekDates] =  React.useState([])
+    const [showNotesEditor,setShowNotesEditor] = React.useState(false)
+
 
     React.useEffect( () => {
         const today = new Date()
- 
-        
         const arr = []; 
 
         for (let i = -3; i <= 3; i++) {
@@ -51,11 +64,16 @@ const JournalPage = () => {
         })
 
         return (
-            <div>
+            <div className="journal-container">
                 <div className="week-days-container">
                     {weekDatesElements}
                 </div>
-                <JournalNote />
+                { !showNotesEditor ? 
+                (<>
+                <img className="journal-art" src={journalArt} alt="journal-art" />
+                <p className="journal-quote">{quote}</p>
+                <button className="create-icon-btn" onClick={() => setShowNotesEditor(true)}></button></>):
+                 <JournalNote /> }
             </div>
     )
 }
