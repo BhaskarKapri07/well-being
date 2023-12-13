86450eabdc2068694cd54f7b255aea9587d62356
Bhaskar Kapri <kapri.b@somaiya.edu>
2023-12-13 23:08:58 +0530

    add Navlinks to Navbar component

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
 import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div>Logo</div>
-      <div>Links</div>
+      <NavLink to="/">
+        <img src={logo} alt="Logo" className="navbar-logo" />
+      </NavLink>
+      <div className="nav-links">
+        <NavLink to="/" activeClassName="active-link" exact>Home</NavLink>
+        <NavLink to="/categories" activeClassName="active-link">Resources</NavLink>
+        <NavLink to="/journal" activeClassName="active-link">Journal</NavLink>
+        <NavLink to="/help" activeClassName="active-link">Talk to an Expert</NavLink>
+        <NavLink to="/login" activeClassName="active-link">LogIn</NavLink>
+      </div>
     </nav>
   );
 }
