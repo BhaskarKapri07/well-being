b59b5ab12cda57265d9e66aff80564defd8421bc
Bhaskar Kapri <kapri.b@somaiya.edu>
2023-12-13 23:10:36 +0530

    add Panic Button logic

--- frontend/src/components/PanicButton/PanicButton.js
+++ frontend/src/components/PanicButton/PanicButton.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './PanicButton.css';
 
 function PanicButton() {
-  const handleClick = () => {
-    console.log('Panic Button Clicked!');
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+  const navigate = useNavigate();
+
+  const messages = [
+    "Take a deep breath...",
+    "Hold for a moment...",
+    "Exhale slowly...",
+    "Feel the tension leaving your body...",
+    "Acknowledge your thoughts, let them pass...",
+    "Focus on the present, you are in this moment...",
+    "Visualize a place where you feel calm and safe...",
+    "Remember, you are valued and strong...",
+    "Breathe in peace, breathe out stress...",
+    "You're doing great, one step at a time..."
+  ];
+
+  useEffect(() => {
+    if (showOverlay && currentMessageIndex < messages.length) {
+      const timer = setTimeout(() => {
+        setCurrentMessageIndex(currentMessageIndex + 1);
+      }, 4000); // Each message displays for 4 seconds
+
+      return () => clearTimeout(timer);
+    } else if (currentMessageIndex === messages.length) {
+      setShowOverlay(false);
+      navigate('/help');
+    }
+  }, [showOverlay, currentMessageIndex, navigate, messages.length]);
+
+  const handlePanicClick = () => {
+    setShowOverlay(true);
+    setCurrentMessageIndex(0);
   };
 
   return (
-    <button className="panic-button" onClick={handleClick}>
-      Panic Button
-    </button>
+    <>
+      <button className="panic-button" onClick={handlePanicClick}>
+        Panic Button
+      </button>
+
+      {showOverlay && (
+        <div className="calming-overlay">
+          <p className="calming-message">{messages[currentMessageIndex]}</p>
+        </div>
+      )}
+    </>
   );
 }
 
