74cb9647ac9a14adef5ca2c8347e1f2d06c488d4
jaswant-rajput <jaswant.s@somaiya.edu>
2023-12-14 04:00:54 +0530

    added alert for accessing journal when not logged in and some minor changes in journal

--- frontend/src/components/JournalNote/JournalNote.js
+++ frontend/src/components/JournalNote/JournalNote.js
@@ -5,6 +5,8 @@ import React from "react"
 import "./JournalNote.css"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
+import UserNotLoggedInAlert from "../UserNotLoggedInAlert/UserNotLoggedInAlert"
+import Toast from "../Toast/Toast"
 
 const JournalNote = () => {
 
@@ -12,15 +14,17 @@ const JournalNote = () => {
       title:"",
       content:""
     })
-
-    
-
-
+    const [showUserNotLoggedInAlert,setShowUserNotLoggedInAlert] = React.useState(false)
+    const [showSavedToast,setShowSavedToast] = React.useState(false)
     const userToken = localStorage.getItem("token")
     const navigate = useNavigate()
     
     const onSave = () => {
-        const requestOptions = {
+        if(userToken){
+          if(note.title && note.content){
+
+          
+          const requestOptions = {
             method: 'post',
             url: `http://localhost:3001/api/journal`,
             headers: {
@@ -34,13 +38,39 @@ const JournalNote = () => {
           };  
 
           axios(requestOptions)
-            .then(response => console.log(response.data))
+            .then(response => {
+              setShowSavedToast(true)
+              navigate(`allNotes/${response.data.entry._id}`)
+              console.log(response.data)
+            })
             .catch(error => console.error(error));
-        
+          }else{
+            console.log("Enter some data ")
+          }
+        }else{
+          setShowUserNotLoggedInAlert(true)
+        }
       }
 
       const handleChange = (e) => {
         setNote({...note,[e.target.name]:e.target.value})
+      }
+
+      const handleUserNotLoggedInClick = () => {
+        setShowUserNotLoggedInAlert(false)
+      }
+
+      const handleToastRemove = () =>{
+        setShowSavedToast(false)
+      }
+
+      const viewAllEntries = () => {
+        if(!userToken){
+          setShowUserNotLoggedInAlert(true)
+        }
+        else {
+          navigate("/journal/allNotes")
+        }
       }
 
     return (
@@ -48,7 +78,9 @@ const JournalNote = () => {
         <input type="text" placeholder="Title..." name="title" value={note.title} onChange={handleChange }/>
         <textarea className="note-content" name="content" value={note.content} onChange={handleChange} id="content"  rows="4"  placeholder="Three things you are grateful for..."></textarea>        
         <button className="note-submit-btn" onClick={onSave} >Save</button>
-        <button onClick={()=>navigate("/journal/allNotes")}> View All Entries</button>   
+        <button onClick={viewAllEntries}> View All Entries</button>   
+        {showUserNotLoggedInAlert && <UserNotLoggedInAlert handleUserNotLoggedInClick={handleUserNotLoggedInClick}/>}
+        {showSavedToast && <Toast msg="Saved" handleToastRemove={handleToastRemove}/>}
     </div>
     )
 }
