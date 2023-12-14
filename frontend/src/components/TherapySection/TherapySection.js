15a44920c41e8178c4f652d5d7df428fba69018b
Bhaskar Kapri <kapri.b@somaiya.edu>
2023-12-14 10:14:43 +0530

    add css for therapy section

--- frontend/src/components/TherapySection/TherapySection.js
+++ frontend/src/components/TherapySection/TherapySection.js
@@ -1,32 +1,29 @@
+import React from "react";
+import therapistImage from "../../assets/images/therapist.jpg";
+import { useNavigate } from "react-router-dom";
+import "./TherapySection.css";
 
+const TherapySection = () => {
+  const navigate = useNavigate();
 
-import React from "react"
-import therapistImage from "../../assets/images/therapist.jpg"
-import { useNavigate } from "react-router-dom"
-import "./TherapySection.css"
+  const handleNavigate = () => {
+    navigate("/help");
+  };
 
-const TherapySection = () =>{
-
-
-    const navigate = useNavigate()
-
-    const handleNavigate = () => {
-        navigate("/help")
-    }
-
-    return (
-        <div className="therapy-section">
-        <div className="therapy-text">
-          <p>
-          "It takes courage to seek help, but it is a sign of strength, not weakness. Talking to a therapist can be a life-changing experience." 
-          </p>
-          <button onClick={handleNavigate}>Explore Therapy</button>
-        </div>
-        <div className="therapy-image">
-          <img src={therapistImage} alt="therapist" />
-        </div>
+  return (
+    <div className="therapy-section">
+      <div className="therapy-image">
+        <img src={therapistImage} alt="therapist" />
       </div>
-    )
-}
+      <div className="therapy-text">
+        <p>
+          "It takes courage to seek help, but it is a sign of strength, not
+          weakness. Talking to a therapist can be a life-changing experience."
+        </p>
+        <button onClick={handleNavigate}>Explore Therapy</button>
+      </div>
+    </div>
+  );
+};
 
-export default TherapySection
+export default TherapySection;
