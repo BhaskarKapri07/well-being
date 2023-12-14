a56fb1ecdad082d85a31b3d0702de567052b3046
Bhaskar Kapri <kapri.b@somaiya.edu>
2023-12-14 14:40:40 +0530

    add css for alert on journal page

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -3,24 +3,21 @@ import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import "./Navbar.css";
 
-
 function Navbar() {
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const navigate = useNavigate();
 
-  const [loggedIn,setLoggedIn] = React.useState(false)
-  const navigate = useNavigate()
-
-  React.useEffect(()=>{
-    const token = localStorage.getItem("token")
-    setLoggedIn(!!token)
-  },[])
+  React.useEffect(() => {
+    const token = localStorage.getItem("token");
+    setLoggedIn(!!token);
+  }, []);
 
   const handleLogout = () => {
     // Implement your logout logic here, e.g., remove the token from local storage
     localStorage.removeItem("token");
     setLoggedIn(false);
-    navigate("/login")
+    navigate("/login");
   };
-
 
   return (
     <nav className="navbar">
@@ -32,7 +29,11 @@ function Navbar() {
         <NavLink to="/resources">Resources</NavLink>
         <NavLink to="/journal">Journal</NavLink>
         <NavLink to="/therapy">Talk to an Expert</NavLink>
-        {loggedIn ? <NavLink onClick={handleLogout}>LogOut</NavLink> : <NavLink to="/login">LogIn</NavLink> }
+        {loggedIn ? (
+          <NavLink onClick={handleLogout}>Logout</NavLink>
+        ) : (
+          <NavLink to="/login">Login</NavLink>
+        )}
       </div>
     </nav>
   );
